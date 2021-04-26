bbcae2345da1e072041b19f3bd6ff04afe644789
Timo Bakx <timobakx@gmail.com>
2021-04-26 16:58:42 +0200

    Added the AfterRead event

---
+++ src/Events/AfterRead/AfterReadEvent.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace TimoBakx\ApiEventsBundle\Events\AfterRead;
+
+use TimoBakx\ApiEventsBundle\Events\BaseDataEvent;
+
+final class AfterReadEvent extends BaseDataEvent
+{
+}
